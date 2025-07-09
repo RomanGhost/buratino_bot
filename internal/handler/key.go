088deff36f258d100ed3d7aed8ba9f372562b420
Ghost <rildiarov@gmail.com>
2025-07-10 01:08:43 +0400

    edit messages

--- internal/handler/key.go
+++ internal/handler/key.go
@@ -2,11 +2,25 @@ package handler
 
 import (
 	"context"
+	"log"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
 )
 
-func CreateKeyInline(ctx context.Context, bot *bot.Bot, mes models.MaybeInaccessibleMessage, data []byte) {
-	// TODO
+func CreateKeyInline(ctx context.Context, b *bot.Bot, mes models.MaybeInaccessibleMessage, data []byte) {
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: mes.Message.Chat.ID,
+		Text: `🔧 *В разработке* 🔮
+
+		Тссс\.\.\. *Буратино колдует над новыми чудесами* 🧙‍♂️✨  
+		Скоро здесь появится нечто волшебное, что поможет тебе ещё проще и быстрее получать тайные ключики от свободного интернета 🌍🔑
+
+		*Потерпи немного, добрый странник* \- магия требует времени\! ⏳`,
+		ParseMode: models.ParseModeMarkdown,
+	})
+
+	if err != nil {
+		log.Printf("[WARN] Error send key message %v", err)
+	}
 }
--- internal/handler/project.go
+++ internal/handler/project.go
@@ -2,11 +2,31 @@ package handler
 
 import (
 	"context"
+	"log"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"github.com/go-telegram/ui/keyboard/inline"
 )
 
-func InfoAboutInline(ctx context.Context, bot *bot.Bot, mes models.MaybeInaccessibleMessage, data []byte) {
-	// TODO
+func InfoAboutInline(ctx context.Context, b *bot.Bot, mes models.MaybeInaccessibleMessage, data []byte) {
+	inlineKeyboard := inline.New(b, inline.WithPrefix("key")).
+		Row().
+		Button("Создать ключ", []byte("create_key"), CreateKeyInline)
+
+	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: mes.Message.Chat.ID,
+		Text: `📜 *Сказ о волшебных ключах* 🗝️
+В этой сказочной обители ты встретил *Буратино* \- не просто деревянного мальчишку, а стража потайных троп интернета\! 🌐✨
+Он дарует *волшебные VPN\-ключи*, что действуют недолго \- всего около *30 минут*, но дают силу обойти коварных Карабасов и Брандмейстеров\.
+Сейчас ключики выдаются через *Outline*, но скоро и *WireGuard* придёт на помощь храбрым странникам\!
+Нажми на волшебную кнопку, и путь будет открыт\.\.\. 🧙‍♂️🔑`,
+		ParseMode:   models.ParseModeMarkdown,
+		ReplyMarkup: inlineKeyboard,
+	})
+
+	if err != nil {
+		log.Printf("[WARN] Error send info message %v", err)
+	}
+
 }
